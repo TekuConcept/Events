eef0563d97939ed0cff27a6e139870466972543e
tekuconcept <electro_madness@yahoo.com>
2018-02-15 03:13:59 +0000

    Updated And Fixed StaticCommand
    StaticCommand's define has been corrected. Also the function type has been updated to support the latest C++11 object management and lambdas

--- Include/StaticCommand.h
+++ Include/StaticCommand.h
@@ -8,24 +8,26 @@
 #include <memory>
 #include "EventCommand.h"
 
+#include <functional>
+
 template <class Args>
 class StaticCommand : public EventCommand<Args> {
 public:
-    typedef void (* Action)(Object& sender, Args e);
-    
-    StaticCommand(Action a);
+    StaticCommand(std::function<void (Object&,Args)> a);
     
     virtual void invoke(Object& sender, Args e) {
-        (*_action_)(sender, e);
+        _action_(sender,e);
     }
     
 private:
-    Action _action_;
+    std::function<void (Object&,Args)> _action_;
 };
 
 template <class Args>
-StaticCommand<Args>::StaticCommand(Action a) : _action_(a) {};
+StaticCommand<Args>::StaticCommand(std::function<void (Object&,Args)> a) :
+    _action_(a) {};
 
 #define StaticCommandPtr(e, f) std::make_shared<StaticCommand<e>>(&f)
+#define LambdaCommandPtr(e, f) std::make_shared<StaticCommand<e>>(f)
 
 #endif
